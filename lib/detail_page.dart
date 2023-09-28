7f44a3831a6dfafd673bbf4f7235d842ba25c94b
Masjoel <masjoel@gmail.com>
2023-09-28 16:44:58 +0700

    update ListView pada detail page

--- lib/detail_page.dart
+++ lib/detail_page.dart
@@ -46,148 +46,112 @@ class _DetailWebPageState extends State<DetailWebPage> {
     // final screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          vertical: 16,
-          horizontal: 64,
-        ),
-        child: Center(
-          child: SizedBox(
-            width: 1200,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                const Text(
-                  'Sedekah Sampah Berkah',
-                  style: TextStyle(
-                    fontFamily: 'Jost',
-                    fontSize: 32,
+      body: ListView(
+        children: [Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 16,
+            horizontal: 64,
+          ),
+          child: Center(
+            child: SizedBox(
+              width: 1200,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  const Text(
+                    'Sedekah Sampah Berkah',
+                    style: TextStyle(
+                      fontFamily: 'Jost',
+                      fontSize: 32,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 32),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Expanded(
-                      child: Column(
-                        children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(10),
-                            child: Image.asset(widget.place.donaturImageAsset),
-                          ),
-                          const SizedBox(height: 16),
-                          Scrollbar(
-                            controller: _scrollController,
-                            child: Container(
-                              height: 150,
-                              padding: const EdgeInsets.only(bottom: 16),
-                              child: ListView(
-                                controller: _scrollController,
-                                scrollDirection: Axis.horizontal,
-                                children: widget.place.imageUrls.map((url) {
-                                  return Padding(
-                                    padding: const EdgeInsets.all(4.0),
-                                    child: ClipRRect(
-                                      borderRadius: BorderRadius.circular(10),
-                                      child: Image.network(url),
-                                    ),
-                                  );
-                                }).toList(),
+                  const SizedBox(height: 32),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        child: Column(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.asset(widget.place.donaturImageAsset),
+                            ),
+                            const SizedBox(height: 16),
+                            Scrollbar(
+                              controller: _scrollController,
+                              child: Container(
+                                height: 150,
+                                padding: const EdgeInsets.only(bottom: 16),
+                                child: ListView(
+                                  controller: _scrollController,
+                                  scrollDirection: Axis.horizontal,
+                                  children: widget.place.imageUrls.map((url) {
+                                    return Padding(
+                                      padding: const EdgeInsets.all(4.0),
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(10),
+                                        child: Image.network(url),
+                                      ),
+                                    );
+                                  }).toList(),
+                                ),
                               ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
-                    const SizedBox(width: 32),
-                    Expanded(
-                      child: Card(
-                        child: Container(
-                          padding: const EdgeInsets.all(16),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.max,
-                            children: <Widget>[
-                              Text(
-                                widget.place.donaturName,
-                                textAlign: TextAlign.center,
-                                style: const TextStyle(
-                                  fontSize: 30.0,
-                                  fontFamily: 'Jost',
-                                ),
-                              ),
-                              // Row(
-                              //   mainAxisAlignment:
-                              //       MainAxisAlignment.spaceBetween,
-                              //   children: [
-                              //     Row(
-                              //       children: <Widget>[
-                              //         const Icon(Icons.calendar_today),
-                              //         const SizedBox(width: 8.0),
-                              //         Text(
-                              //           widget.place.openDays,
-                              //           style: informationTextStyle,
-                              //         ),
-                              //       ],
-                              //     ),
-                              //     const FavoriteButton(),
-                              //   ],
-                              // ),
-                              // Row(
-                              //   children: <Widget>[
-                              //     const Icon(Icons.access_time),
-                              //     const SizedBox(width: 8.0),
-                              //     Text(
-                              //       widget.place.openTime,
-                              //       style: informationTextStyle,
-                              //     ),
-                              //   ],
-                              // ),
-                              // const SizedBox(height: 8.0),
-                              // Row(
-                              //   children: <Widget>[
-                              //     const Icon(Icons.monetization_on),
-                              //     const SizedBox(width: 8.0),
-                              //     Text(
-                              //       widget.place.ticketPrice,
-                              //       style: informationTextStyle,
-                              //     ),
-                              //   ],
-                              // ),
-                              Container(
-                                padding:
-                                    const EdgeInsets.symmetric(vertical: 8.0),
-                                child: Text(
-                                  widget.place.donaturLocation,
-                                  textAlign: TextAlign.justify,
+                      const SizedBox(width: 32),
+                      Expanded(
+                        child: Card(
+                          child: Container(
+                            padding: const EdgeInsets.all(16),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.max,
+                              children: <Widget>[
+                                Text(
+                                  widget.place.donaturName,
+                                  textAlign: TextAlign.center,
                                   style: const TextStyle(
-                                    fontSize: 14.0,
+                                    fontSize: 30.0,
                                     fontFamily: 'Jost',
                                   ),
                                 ),
-                              ),
-                              Container(
-                                padding:
-                                    const EdgeInsets.symmetric(vertical: 16.0),
-                                child: Text(
-                                  'Pesan : ${widget.place.donaturNote}',
-                                  textAlign: TextAlign.justify,
-                                  style: const TextStyle(
-                                    fontSize: 16.0,
-                                    fontFamily: 'Jost',
+                                Container(
+                                  padding:
+                                      const EdgeInsets.symmetric(vertical: 8.0),
+                                  child: Text(
+                                    widget.place.donaturLocation,
+                                    textAlign: TextAlign.justify,
+                                    style: const TextStyle(
+                                      fontSize: 14.0,
+                                      fontFamily: 'Jost',
+                                    ),
                                   ),
                                 ),
-                              ),
-                            ],
+                                Container(
+                                  padding:
+                                      const EdgeInsets.symmetric(vertical: 16.0),
+                                  child: Text(
+                                    'Pesan : ${widget.place.donaturNote}',
+                                    textAlign: TextAlign.justify,
+                                    style: const TextStyle(
+                                      fontSize: 16.0,
+                                      fontFamily: 'Jost',
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
+        ),]
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
